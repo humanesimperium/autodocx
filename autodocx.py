15d44d95a8dad287a8d795304f796ec82c6b7b07
Alex <100867066+ecklex@users.noreply.github.com>
2024-04-19 10:54:15 +0200

    Add files via upload

--- autodocx.py
+++ autodocx.py
@@ -1,129 +1,59 @@
-import os
 from docx import Document
 
 
-# _______ pre section _______
+def docx_to_xml(docx_path, xml_path):
+    doc = Document(docx_path)
+    xml_content = ""
 
-# Defining directory containing the documents
-directory = '/path/to/your/directory/'
+    # Built-in Styles to convert
+    for paragraph in doc.paragraphs:
+        if paragraph.style.name == 'Title':
+            xml_content += "<title>{}</title>".format(paragraph.text)
 
-# Iterate over all files in the directory
-for filename in os.listdir(directory):
-    # Check if the file is a .docx file
-    if filename.endswith(".docx"):
-        # Construct the full path to the document
-        filepath = os.path.join(directory, filename)
-        # Open the document
-        document = Document(filepath)
+        elif paragraph.style.name == 'Subtitle':
+            xml_content += "<subtitle>{}</subtitle>".format(paragraph.text)
 
-# Defining styles, needed for the more complicated and custom styles (e.g. lists, bold, italic, etc.) so far.
-styles = document.styles
+        elif paragraph.style.name == 'Heading 1':
+            xml_content += "<h1>{}</h1>".format(paragraph.text)
 
-# _______ pre section end _______
+        elif paragraph.style.name == 'Heading 2':
+            xml_content += "<h2>{}</h2>".format(paragraph.text)
 
+        elif paragraph.style.name == 'Heading 3':
+            xml_content += "<h3>{}</h3>".format(paragraph.text)
 
-# _______ built-in section _______
+        elif paragraph.style.name == 'Heading 4':
+            xml_content += "<h4>{}</h4>".format(paragraph.text)
 
-# Following loops iterate for Word built-in-styles and adds a corresponding <tags>. looping for
-#   Title
-#   Subtitle
-#   Heading 1; Heading 2; Heading 3; Heading 4; Heading 5; Heading 6;
-#   Normal
-# 
+        elif paragraph.style.name == 'Heading 5':
+            xml_content += "<h5>{}</h5>".format(paragraph.text)
 
-# Title
-for paragraph in document.paragraphs:
-    if paragraph.style.name == 'Title':
-        paragraph.text = '<?xml version="1.0" encoding="UTF-8"?><doc><meta><title>' + paragraph.text + '</title>'
+        elif paragraph.style.name == 'Heading 6':
+            xml_content += "<h6>{}</h6>".format(paragraph.text)
 
-# Subtitle
-for paragraph in document.paragraphs:
-    if paragraph.style.name == 'Subtitle':
-        paragraph.text = '<subtitle>' + paragraph.text + '</subtitle>'
+        elif paragraph.style.name == 'Normal':
+            xml_content += "<p>{}</p>".format(paragraph.text)
 
-# Heading 1
-for paragraph in document.paragraphs:
-    if paragraph.style.name == 'Heading 1':
-        paragraph.text = '<h1>' + paragraph.text + '</h1>'
+    ### custom styles to convert. enter the exact style name as it appears in the docx file
 
-# Heading 2
-for paragraph in document.paragraphs:
-    if paragraph.style.name == 'Heading 2':
-        paragraph.text = '<h2>' + paragraph.text + '</h2>'
+        # List Bullet (unordered list)
+        elif paragraph.style.name == 'ListBullet':
+            xml_content += "<ul><li>{}</li></ul>".format(paragraph.text)
 
-# Heading 3
-for paragraph in document.paragraphs:
-    if paragraph.style.name == 'Heading 3':
-        paragraph.text = '<h3>' + paragraph.text + '</h3>'
+        # List Number
+        elif paragraph.style.name == 'Listenabsatz1':
+            xml_content += "<ol><li>{}</li></ol>".format(paragraph.text)
 
-# Heading 4
-for paragraph in document.paragraphs:
-    if paragraph.style.name == 'Heading 4':
-        paragraph.text = '<h4>' + paragraph.text + '</h4>'
-
-# Heading 5
-for paragraph in document.paragraphs:
-    if paragraph.style.name == 'Heading 5':
-        paragraph.text = '<h5>' + paragraph.text + '</h5>'
-
-# Heading 6
-for paragraph in document.paragraphs:
-    if paragraph.style.name == 'Heading 6':
-        paragraph.text = '<h6>' + paragraph.text + '</h6>'
-
-# Normal
-for paragraph in document.paragraphs:
-    if paragraph.style.name == 'Normal':
-        paragraph.text = '<p>' + paragraph.text + '</p>'
-
-# _______ built-in section end _______
+### Converting Italic and Bold. Doesnt work yet
 
 
-# _______ custom style section _______
 
-# Following loops iterate for Word custom styles (styles created by the user) and lists and adds desired <tags>. Loops must be given the exact style name from the word doc. <tags> must be defined in the "paragraph.text" argument.
+    with open(xml_path, "w") as xml_file:
+        xml_file.write("<document>{}</document>".format(xml_content))
 
-# List Bullet ('Aufzählungszeichen1') und formatiert den paragraphen als "normalen" Text und fügt "<ul>" davor und "</ul>" danach ein.
-for paragraph in document.paragraphs:
-    if paragraph.style.name == 'Aufzählungszeichen1':
-        paragraph.style = styles['Normal'] # could be that this is of no use, if the Lists can be converted with built-in-Style. The "styles = document.styles" argument could be deleted if so.
-        paragraph.text = '<ul>' + paragraph.text + '</ul>'
+# Replace 'input.docx' with the path to your input .docx file
+# Replace 'output.xml' with the path where you want to save the XML file
+docx_to_xml('input.docx', 'output.xml')
 
-# List Number ('Listenabsatz1') und formatiert den paragraphen als "normalen" Text und fügt "<ol>" davor und "</ol>" danach ein.
-for paragraph in document.paragraphs:
-    if paragraph.style.name == 'Listenabsatz1':
-        paragraph.style = styles['Normal'] # could be that this is of no use, if the Lists can be converted with built-in-Style. The "styles = document.styles" argument could be deleted if so.
-        paragraph.text = '<ol>' + paragraph.text + '</ol>'
-
-# Italic ('Standard kursiv') und fügt vor der Run "<italic>" ein und danach "</italic>"
-for paragraph in document.paragraphs:
-    for run in paragraph.runs:
-        if run.italic:
-            run.text = '<italic>' + run.text + '</italic>'
-
-# work in progress
-# durchläuft den Text und sucht nach fettgedrucktem Text und fügt davor "<bold>" ein und danach "</bold>"
-for paragraph in document.paragraphs:
-    for run in paragraph.runs:
-        if run.bold:
-            run.text = '<bold>' + run.text + '</bold>'
-
-# author
-for paragraph in document.paragraphs:
-    if paragraph.style.name == 'author':
-        paragraph.text = '<author>' + paragraph.text + '</author></meta>'
-
-# doc_end
-for paragraph in document.paragraphs:
-    if paragraph.style.name == 'doc_end':
-        paragraph.text = '</doc>'
-
-# _______ custom style section end _______
-
-
-# _______ post section _______
-
-# save edited file to a output directory
-document.save('/path/to/your/directory/' + filename) # Don't forget to change the path to your desired output directory.
-
-# _______ post section end _______
+exec(open('/mnt/c/Users/aecke/Desktop/python/autodocx/subprocesses/clean_lists.py').read())
+exit()
