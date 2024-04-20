08a27fdd4bfa471b276c80b7f0e298ce8fd54e4c
Alex <100867066+ecklex@users.noreply.github.com>
2024-04-20 14:46:18 +0200

    Update clean_lists.py

--- subprocesses/clean_lists.py
+++ subprocesses/clean_lists.py
@@ -1,12 +1,14 @@
+# clean up for not entirely well made conversion of the main script
+
 import re
 
 # opens a specific xml file, parse through and search for occurences of "</ul>\n<ul>" and "</ol>\n<ol>" and remove them
-with open("/mnt/c/Users/aecke/Desktop/python/autodocx/output.xml", "r") as file:
+with open("path/to/your/file/XY.xml", "r") as file:
     content = file.read()
     content = re.sub(r"</ul>\s*<ul>", "", content, flags=re.DOTALL)
     content = re.sub(r"</ol>\s*<ol>", "", content, flags=re.DOTALL)
 
 # save the cleaned xml file
-with open("/mnt/c/Users/aecke/Desktop/python/autodocx/output.xml", "w") as file:
+with open("path/to/your/file/XY.xml", "w") as file:
     file.write(content)
 
