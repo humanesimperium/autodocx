e3700d2d77d24e664778bb2347697f0c920f6af5
Alex <100867066+ecklex@users.noreply.github.com>
2024-04-26 11:37:34 +0200

    Add files via upload

--- autodocx.py
+++ autodocx.py
@@ -1,58 +1,62 @@
 from docx import Document
 
-
 def docx_to_xml(docx_path, xml_path):
     doc = Document(docx_path)
     xml_content = ""
 
     # Built-in Styles to convert
     for paragraph in doc.paragraphs:
-        if paragraph.style.name == 'Title':
-            xml_content += "<title>{}</title>".format(paragraph.text)
+        style_name = paragraph.style.name
+        text = paragraph.text
+        
+        # title
+        if style_name == 'Title':
+            xml_content += "<title>{}</title>".format(text)
 
-        elif paragraph.style.name == 'Subtitle':
-            xml_content += "<subtitle>{}</subtitle>".format(paragraph.text)
+        # subtitle
+        elif style_name == 'Subtitle':
+            xml_content += "<subtitle>{}</subtitle>".format(text)
 
-        elif paragraph.style.name == 'Heading 1':
-            xml_content += "<h1>{}</h1>".format(paragraph.text)
+        # headings
+        elif style_name.startswith('Heading'):
+            level = style_name.split()[1]
+            xml_content += "<h{}>{}</h{}>".format(level, text, level)
 
-        elif paragraph.style.name == 'Heading 2':
-            xml_content += "<h2>{}</h2>".format(paragraph.text)
+        # normal text
+        elif style_name == 'Normal':
+            xml_content += "<p>{}</p>".format(text)
 
-        elif paragraph.style.name == 'Heading 3':
-            xml_content += "<h3>{}</h3>".format(paragraph.text)
-
-        elif paragraph.style.name == 'Heading 4':
-            xml_content += "<h4>{}</h4>".format(paragraph.text)
-
-        elif paragraph.style.name == 'Heading 5':
-            xml_content += "<h5>{}</h5>".format(paragraph.text)
-
-        elif paragraph.style.name == 'Heading 6':
-            xml_content += "<h6>{}</h6>".format(paragraph.text)
-
-        elif paragraph.style.name == 'Normal':
-            xml_content += "<p>{}</p>".format(paragraph.text)
-
-    ### custom styles to convert. enter the exact style name as it appears in the docx file
+    # custom styles to convert. enter the exact style name as it appears in the docx file
 
         # List Bullet (unordered list)
-        elif paragraph.style.name == 'ListBullet':
-            xml_content += "<ul><li>{}</li></ul>".format(paragraph.text)
+        elif style_name == 'ListBullet':
+            xml_content += "<ul><li>{}</li></ul>".format(text)
 
         # List Number
-        elif paragraph.style.name == 'Listenabsatz1':
-            xml_content += "<ol><li>{}</li></ol>".format(paragraph.text)
+        elif style_name == 'Listenabsatz1':
+            xml_content += "<ol><li>{}</li></ol>".format(text)
 
-### Converting Italic and Bold. Doesnt work yet
+        # add more custom styles here  
 
-# save converted file
+        # bold text
+        for run in paragraph.runs:
+            if run.style.name == 'fett':
+                xml_content = xml_content.replace(run.text, "<b>{}</b>".format(run.text))
+
+        # italic text (for italic text, search for custom style, due to a word bug, italic itself is not recognized)
+        for run in paragraph.runs:
+            if run.style.name == 'kursiv':
+                xml_content = xml_content.replace(run.text, "<i>{}</i>".format(run.text))
+
+
+    # save xml file
     with open(xml_path, "w") as xml_file:
         xml_file.write("<document>{}</document>".format(xml_content))
 
-# Replace 'input.docx' with the path to your input .docx file
+# Replace 'input.docx' with the path to your input.docx file
 # Replace 'output.xml' with the path where you want to save the XML file
 docx_to_xml('input.docx', 'output.xml')
 
-exec(open('path/to/the/repository/autodocx/subprocesses/clean_lists.py').read())
+# some script to clean the lists in the xml file
+exec(open('/mnt/c/Users/aecke/Desktop/python/autodocx/subprocesses/clean_lists.py').read())
 exit()
